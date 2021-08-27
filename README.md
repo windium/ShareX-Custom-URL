20987e87ee11680abb8da6e81e63d239d9a4d28f
Windium <43916344+Windium@users.noreply.github.com>
2021-08-27 20:22:30 +0300

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# ShareX-Custom-URL
+# ShareX Custom URL
 Cloudflare Workers script that allows you to upload files to ShareX Custom Uploaders and use with your own domain.
+
+- You need to use Cloudflare as a DNS service to connect your worker to domain.
+
+## Installation
+
+Clone this repo using git `git clone https://github.com/Windium/ShareX-Custom-URL.git`
+
+### Cloudflare Worker
+Create worker account and worker. [guide](https://developers.cloudflare.com/workers/quickstart)
+
+Open `worker-script` on any text exitor change `cdnDomain` to custom uploader url and `domain` to your domain.
+
+### ShareX Config
+  - Open ShareX > Destinations > Custom uploader settings > Import > From file > Select `custom-domain.sxcu` file > Open
+  - On custom domain settings change request url to your domain.
+  - Go back to ShareX menu > Destinations > URL Shortener > Select Custom URL Shortener
+  - After capture tasks must be selected as `Upload image to host`.
+  - After upload tasks select `Shorten URL` and `Copy URL to clipboard`.
---
+++ workers-script.js
@@ -0,0 +1,44 @@
+addEventListener('fetch', event => {
+    event.respondWith(runScript(event));
+})
+let cdnDomain = "example-sharex-uploader.com"
+let domain = "cdn.example.com"
+async function runScript(event) {
+    let response;
+    if(event.request.method == 'POST') {
+        let json = await event.request.json();
+        response = json.url.replace(cdnDomain, domain)
+        return new Response(response)
+    } else {
+        let regex = /[a-zA-Z0-9]+\.(png|jpe?g|gifv?)/
+        let status;
+        const urlString = new URL(event.request.url).pathname.substring(1);
+
+        if(!urlString) {
+            status = 400
+            return new Response(
+                JSON.stringify({
+                    error: "400: Please specify image url"
+                }), {
+                    status: status
+                }
+            )
+        }
+
+        if(!regex.test(urlString)) {
+            status = 404
+            return new Response(
+                JSON.stringify({
+                    error: "404: Not found"
+                }), {
+                    status: status
+                }
+            )
+        }
+
+        let img = await fetch("https://" + cdnDomain + "/" + urlString)
+        let clone = img.clone().body
+        response = new Response(clone)
+    }
+    return response
+}
